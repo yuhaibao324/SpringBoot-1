578dfa5116a2bea60b8c70c0011faf0ab154c8d5
正义之剑 <yuhaibao324@126.com>
2018-05-29 08:29:23 +0800

    测试类

--- SpringBoot--SpringData-多数据源/src/test/java/com/example/DemoApplicationTests.java
+++ SpringBoot--SpringData-多数据源/src/test/java/com/example/DemoApplicationTests.java
@@ -11,7 +11,7 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = DemoApplication.class)
+@SpringApplicationConfiguration(classes = DemoApplicationTests.class)
 public class DemoApplicationTests {
 
 	@Autowired
